79038ef539be4831c60dfbbf09c8eb554ffbf583
Deya Card <thedeyacard@gmail.com>
2022-08-02 14:45:26 -0700

    add data callback functions

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,11 +1,42 @@
 import React from "react";
+import { v4 } from 'uuid';
+import PropTypes from "prop-types";
 
 function NewTicketForm(props){
+
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    props.onNewTicketCreation({
+      names: event.target.names.value, 
+      location: event.target.location.value, 
+      issue: event.target.issue.value, 
+      id: v4()
+      // numberOfStudents: parseInt(event.target.numberOfStudents.value)
+    });
+  }
+
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleNewTicketFormSubmission}>
+        <input
+          type='text'
+          name='names'
+          placeholder='Event name' />
+        <input
+          type='text'
+          name='location'
+          placeholder='Location' />
+        <input
+          type='text'
+          placeholder='Section' />
+        <button type='submit'>Add to cart</button>
+      </form>
     </React.Fragment>
   );
 }
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketForm;
--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,37 +1,43 @@
+
 import React from "react";
 import Ticket from "./Ticket";
+import PropTypes from "prop-types";
 
-const mainTicketList = [
-  {
-    names: 'Warpaint',
-    location: 'Revolution Hall',
-    issue: 'Firebase won\'t save record. Halp.'
-  },
-  {
-    names: 'Sleater-Kinney',
-    location: 'Crystal Ballroom',
-    issue: 'Prop types are throwing an error.'
-  },
-  {
-    names: 'Tobin Heath and OL Reign',
-    location: 'Providence Park',
-    issue: 'Child component isn\'t rendering.'
-  }
-];
+// const mainTicketList = [
+//   {
+//     names: 'Warpaint',
+//     location: 'Revolution Hall',
+//     issue: 'Firebase won\'t save record. Halp.'
+//   },
+//   {
+//     names: 'Sleater-Kinney',
+//     location: 'Crystal Ballroom',
+//     issue: 'Prop types are throwing an error.'
+//   },
+//   {
+//     names: 'Tobin Heath and OL Reign',
+//     location: 'Providence Park',
+//     issue: 'Child component isn\'t rendering.'
+//   }
+// ];
 
-function TicketList(){
+function TicketList(props){
   return (
     <React.Fragment>
      <hr/>
-        {mainTicketList.map((ticket, index) =>
+        {props.ticketList.map((ticket, index) =>
+        // {mainTicketList.map((ticket, index) =>
           <Ticket names={ticket.names}
             location={ticket.location}
             issue={ticket.issue}
-            key={index}/>
+            key={index} />
         )}
     </React.Fragment>
   );
 }
 
+TicketList.propTypes = {
+  ticketList: PropTypes.array
+};
 
 export default TicketList;
